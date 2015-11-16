4676c1bcaf82775d5f1542b5bbf22257e9f1426d
David Fallis <dfallis@uvic.ca>
2015-11-16 12:53:47 -0800

    cleaned up plotload.py

--- plotload.py
+++ plotload.py
@@ -10,30 +10,9 @@ def _scale_units(units, scale):
         units = units + ' * ' + str(scale)
     return units
        
-
-def timeaverage_load(ifile, var, depth_type, dates, realm, scale):
-
-    path, ifile = os.path.split(ifile)
-    if dates:
-        if not os.path.isfile('remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc'):
-            out='remapfiles/remapfile.nc'
-            if realm == 'ocean':
-                cdo.mul(input='-divc,100 mask/ocean ' + path + '/' + ifile, output=out)
-            elif realm == 'land':
-                cdo.mul(input='-divc,100 mask/land ' + path + '/' + ifile, output=out)
-            else:
-                out = path + '/' + ifile
-            cdo.remapdis('r360x180', input='-setctomiss,0 -timmean -seldate,' + str(dates['start_date']) + ',' + str(dates['end_date']) + ' ' + out, output='remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc')
-        nc = Dataset('remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc', 'r')
-    else:
-        if not os.path.isfile('remapfiles/remap_' + ifile):
-            cdo.remapdis('r360x180', input='-timmean ' + path + '/' + ifile, output='remapfiles/remap_' + ifile)
-        nc = Dataset('remapfiles/remap_' + ifile, 'r')
-    
-    
+def _load(nc, var, depth_type):
     try:
         ncvar = nc.variables[var]
-
     except:
         varu = var.upper()
         ncvar = nc.variable[varu]
@@ -53,14 +32,39 @@ def timeaverage_load(ifile, var, depth_type, dates, realm, scale):
                     break
         except:
             depth = [0]
-    
-    #lon = np.linspace(0,359, 360)
-    #lat = np.linspace(-90,90,180)
+    return data, units, depth
+
+def _load2(data, nc, units, depth, scale):
     lon = nc.variables['lon'][:].squeeze()
     lat = nc.variables['lat'][:].squeeze()
     depth = np.round(depth)
     units = _scale_units(units, scale)
-    data = data*scale
+    data = data*scale    
+    return data, lon, lat, depth, units
+    
+def timeaverage_load(ifile, var, depth_type, dates, realm, scale):
+
+    path, ifile = os.path.split(ifile)
+    if dates:
+        if not os.path.isfile('remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc'):
+            out='remapfiles/remapfile.nc'
+            if realm == 'ocean':
+                cdo.mul(input='-divc,100 mask/ocean ' + path + '/' + ifile, output=out)
+            elif realm == 'land':
+                cdo.mul(input='-divc,100 mask/land ' + path + '/' + ifile, output=out)
+            else:
+                out = path + '/' + ifile
+            cdo.remapdis('r360x180', input='-setctomiss,0 -timmean -seldate,' + str(dates['start_date']) + ',' + str(dates['end_date']) + ' ' + out, output='remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc')
+        nc = Dataset('remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc', 'r')
+    else:
+        if not os.path.isfile('remapfiles/remap_' + ifile):
+            cdo.remapdis('r360x180', input='-timmean ' + path + '/' + ifile, output='remapfiles/remap_' + ifile)
+        nc = Dataset('remapfiles/remap_' + ifile, 'r')
+    
+    data, units, depth = _load(nc, var, depth_type)    
+    
+    data, lon, lat, depth, units = _load2(data, nc, units, depth, scale)
+
     return data, units, lon, lat, depth
 
 def timeaverage_load_comp(ifile, var, depth_type, dates, realm, depthneeded, scale):
@@ -72,7 +76,6 @@ def timeaverage_load_comp(ifile, var, depth_type, dates, realm, depthneeded, sca
 
     if dates:
         if not os.path.isfile('remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc'):
-            print 'got here'
             cdo.selvar(var, input=path + '/' + ifile, output='remapfiles/selvar.nc')
             out='remapfiles/selvar.nc'
             cdo.remapdis('r360x180', options='-L', input='-setctomiss,0 -timmean -seldate,' + str(dates['start_date']) + ',' + str(dates['end_date']) + ' ' + out, output='remapfiles/remap_' + ifile + str(dates['start_date']) + str(dates['end_date']) + '.nc')
@@ -92,31 +95,7 @@ def timeaverage_load_comp(ifile, var, depth_type, dates, realm, depthneeded, sca
             except:
                 nc = Dataset('remapfiles/remap_' + ifile, 'r')          
 
-    
-    for key in nc.variables:
-        print '>>>' + key
-    try:
-        ncvar = nc.variables[var]
-
-    except:
-        varu = var.upper()
-        ncvar = nc.variable[varu]
-    data = ncvar[:].squeeze()
-    try:
-        units = ncvar.units
-    except:
-        units = ''
-    
-    depth = [0]
-    if depth_type != "":
-        try:
-            for dimension in ncvar.dimensions:
-                if 'level' in dimension.lower():
-                    depth = nc.variables[dimension][:]
-                    break
-        except:
-            depth = [0]  
-    print depth 
+    data, units, depth = _load(nc, var, 'level') 
     #lon = np.linspace(0,359, 360)
     #lat = np.linspace(-90,90,180)
     lon = nc.variables['lon'][:].squeeze()
@@ -142,40 +121,17 @@ def trends_load(ifile, var, depth_type, dates, scale):
                          + 'trendfiles/slope_' + ifile, output='trendfiles/slope_remap_' + ifile) 
         nc = Dataset('trendfiles/slope_remap_' + ifile, 'r' )   
     
-    ncvar = nc.variables[var]
-    data = ncvar[:].squeeze() 
-
-    try:
-        units = ncvar.units
-    except:
-        units = '' 
+    data, units, depth = _load(nc, var, depth_type) 
       
-    depth = [0]
-    if depth_type != "":
-        try:
-            for dimension in ncvar.dimensions:
-
-                if depth_type in dimension.lower():
-                    depth = nc.variables[dimension][:]
-                    break
-        except:
-            depth = [0]     
-    #lon = np.linspace(0, 359, 360)
-    #lat = np.linspace(-90,90,180)
-    lon = nc.variables['lon'][:].squeeze()
-    lat = nc.variables['lat'][:].squeeze()
-
-    depth = np.round(depth)
-    units = _scale_units(units, scale)
-    return data*scale, units, lon, lat, depth    
+    data, lon, lat, depth, units = _load2(data, nc, units, depth, scale)
+    
+    return data, units, lon, lat, depth    
 
 def trends_load_comp(ifile, var, depth_type, dates, depthneeded, scale):
 
     depthneeded = ["%.2f" % number for number in depthneeded]
     for i in xrange(len(depthneeded)):
         depthneeded[i] = str(depthneeded[i])
-        print type(depthneeded[i])
-    print depthneeded
     depthneededstr = ','.join(depthneeded)
     
     path, ifile = os.path.split(ifile)
@@ -200,32 +156,9 @@ def trends_load_comp(ifile, var, depth_type, dates, depthneeded, scale):
                          + 'trendfiles/slope_' + ifile, output='trendfiles/slope_remap_' + ifile) 
         nc = Dataset('trendfiles/slope_remap_' + ifile, 'r' )   
     
-    ncvar = nc.variables[var]
-    data = ncvar[:].squeeze() 
-
-    try:
-        units = ncvar.units
-    except:
-        units = '' 
-      
-    depth = [0]
-    if depth_type != "":
-        try:
-            for dimension in ncvar.dimensions:
-
-                if depth_type in dimension.lower():
-                    depth = nc.variables[dimension][:]
-                    break
-        except:
-            depth = [0]     
-    #lon = np.linspace(0, 359, 360)
-    #lat = np.linspace(-90,90,180)
-    lon = nc.variables['lon'][:].squeeze()
-    lat = nc.variables['lat'][:].squeeze()
-
-    depth = np.round(depth)
-    units = _scale_units(units, scale)
-    return data*scale, units, lon, lat, depth  
+    data, units, depth = _load(nc, var, 'level')    
+    data, lon, lat, depth, units = _load2(data, nc, units, depth, scale)
+    return data, units, lon, lat, depth  
 
    
 def timeseries_load(ifile, var, depth_type, dates, scale):
@@ -241,32 +174,8 @@ def timeseries_load(ifile, var, depth_type, dates, scale):
         if not os.path.isfile('fldmeanfiles/fldmean_' + ifile):
             cdo.fldmean(input=path + '/' + ifile, output='fldmeanfiles/fldmean_' + ifile)
         nc = Dataset('fldmeanfiles/fldmean_' + ifile, 'r')
-
-    
-
    
-    try:
-        ncvar = nc.variables[var]
-
-    except:
-        varu = var.upper()
-        ncvar = nc.variable[varu]
-    data = ncvar[:].squeeze()
-
-    try:
-        units = ncvar.units
-    except:
-        units = ''
-    
-    depth = [0]
-    if depth_type != "":
-        try:
-            for dimension in ncvar.dimensions:
-                if depth_type in dimension.lower():
-                    depth = nc.variables[dimension][:]
-                    break
-        except:
-            depth = [0]
+    data, units, depth = _load(nc, var, depth_type) 
     
     nc_time = nc.variables['time']
     try: 
@@ -292,31 +201,8 @@ def zonal_load(ifile, var, depth_type, dates, scale):
     else:
         if not os.path.isfile('zonalfiles/zonmean_' + ifile):
             cdo.zonmean(input='-timmean' + path + '/' + ifile, output='zonalfiles/zonmean_' + ifile)
-        nc = Dataset('zonalfiles/zonmean_' + ifile, 'r')
-
-   
-    try:
-        ncvar = nc.variables[var]
-
-    except:
-        varu = var.upper()
-        ncvar = nc.variable[varu]
-    data = ncvar[:].squeeze()
-
-    try:
-        units = ncvar.units
-    except:
-        units = ''
-    
-    depth = [0]
-    if depth_type != "":
-        try:
-            for dimension in ncvar.dimensions:
-                if depth_type in dimension.lower():
-                    depth = nc.variables[dimension][:]
-                    break
-        except:
-            depth = [0]
+        nc = Dataset('zonalfiles/zonmean_' + ifile, 'r')   
+    data, units, depth = _load(nc, var, depth_type) 
     
     x = nc.variables['lat'][:].squeeze()
 
