671505da3b7c528f7c4caae13131c842f733dcc0
David Fallis <dfallis@uvic.ca>
2015-11-16 11:36:06 -0800

    control.py cleaned up

--- control.py
+++ control.py
@@ -1,13 +1,13 @@
-import directory_tools as dt
-import plotter as pltr
-import pdforganize as org
-import defaults as dft
+from directory_tools import getfiles, remfiles
+from plotter import loop
+from pdforganize import arrange
+from defaults import fill
 
 def execute(plots, run, obs, defaults, delete):
-    filled_plots = dft.fill(plots, obs, defaults)
-    plots_with_files = dt.getfiles(filled_plots, run)    
-    plotnames = pltr.loop(plots_with_files)
-    dt.remfiles(**delete)
-    org.arrange(plotnames)
+    filled_plots = fill(plots, obs, defaults)
+    plots_with_files = getfiles(filled_plots, run)    
+    plotnames = loop(plots_with_files)
+    remfiles(**delete)
+    arrange(plotnames)
     
         
