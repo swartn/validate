29beb3dde0d64a92e3b5759f85197dc196508b0a
David Fallis <dfallis@uvic.ca>
2016-01-11 16:26:51 -0800

    one source version

--- setup.py
+++ setup.py
@@ -3,20 +3,21 @@ try:
 except ImportError:
     from distutils.core import setup
 
+from validate import __version__
 
 
 setup(
-    name='validate',
-    version='0.1',
-    author='David W. Fallis',
-    author_email='davidwfallis@gmail.com',
-    packages=['validate'],
-    include_package_data=True,
-    scripts=['bin/validate-configure', 'bin/validate-execute'],
-    url='',
-    description='Model validation package.',
-    long_description=open('README.rst').read(),
-    install_requires=[
+    name = 'validate',
+    version = __version__,
+    author = 'David W. Fallis',
+    author_email = 'davidwfallis@gmail.com',
+    packages = ['validate'],
+    include_package_data = True,
+    scripts = ['bin/validate-configure', 'bin/validate-execute'],
+    url = '',
+    description = 'Model validation package.',
+    long_description = open('README.rst').read(),
+    install_requires = [
         'brewer2mpl >=1.4.1',
         'cdo >=1.2.5',
         'netCDF4 >=1.1.6',
